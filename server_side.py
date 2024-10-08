3e8a05dbaf1430853e22fe5b434bad4632e4167e
Cole Aydelottte <cole.aydelotte@gmail.com>
2024-10-08 18:58:40 -0400

    Created two files for server and client.
    Created two files for server and client.

---
+++ server_side.py
@@ -0,0 +1,8 @@
+def ping():
+    return "pong"
+
+def main():
+    print(ping())
+
+if __name__ == "__main__":
+    main()
---
+++ user_side.py
@@ -0,0 +1,8 @@
+def ping():
+    return "ping"
+
+def main():
+    print(ping())
+
+if __name__ == "__main__":
+    main()
