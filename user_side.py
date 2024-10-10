340f16c43377c49f826466daf879c79f6c6f4300
Cole Aydelotte <aydelottec@moravian.edu>
2024-10-10 14:01:32 -0400

    Simple Client side connection.
    Simple Client side connection.

--- user_side.py
+++ user_side.py
@@ -1,5 +1,9 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
 def ping():
-    return "ping"
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 8080)
+    client_socket.connect(server_address)
 
 def main():
     print(ping())
