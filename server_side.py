97fabe8d72bdd0629befa8a7ac45bf3c0fa2e779
Cole Aydelotte <aydelottec@moravian.edu>
2024-10-18 13:41:53 -0400

    Added class functionality for the server side.
    Added class functionality for the server side.

--- server_side.py
+++ server_side.py
@@ -1,44 +1,49 @@
 from socket import socket, AF_INET, SOCK_STREAM
 
-def create_server_side_connection():
-    # `AF_INET`` stands for "Address Family: Internet".
-    # IPv4 addresses so we are using the internet family.
-    # `SOCK_STREAM` stands for "Socket Type: Stream"
-    # TCP is a stream-oriented protocol, meaning that the data is transmitted in a continuous stream.
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    messages = []
+class Server():
 
-    # Port `8080` listening on ip: `localhost`
-    server_address = ('localhost', 8080)
-    server_socket.bind(server_address)
-    #listening for incoming connections allowing `1` to be queued for connection.
-    server_socket.listen(1)
-    print("Server is listening on port 8080...")
+    def __init__(self):
+        self.messages = []
 
-    while True:
-        # Wait for a connection
-        print("Waiting for a connection...")
-        connection, client_address = server_socket.accept()
-        
-        try:
-            print(f"Connection from {client_address}")
+    def create_server_side_connection(self):
+        # `AF_INET`` stands for "Address Family: Internet".
+        # IPv4 addresses so we are using the internet family.
+        # `SOCK_STREAM` stands for "Socket Type: Stream"
+        # TCP is a stream-oriented protocol, meaning that the data is transmitted in a continuous stream.
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        messages = []
 
-            while True:
-                data = connection.recv(1024)  # Buffer size is 1024 bytes
-                if data:
-                    message = data.decode('utf-8') # When data is received it is sent in bytes, so we decode it to utf-8
-                    print(f"Received: {message}")
-                    messages.append(message)  # Append to the display list
-                else:
-                    print(f"Message list: {messages}")
-                    break
-        
-        finally:
-            connection.close()
+        # Port `8080` listening on ip: `localhost`
+        server_address = ('localhost', 8080)
+        server_socket.bind(server_address)
+        #listening for incoming connections allowing `1` to be queued for connection.
+        server_socket.listen(1)
+        print("Server is listening on port 8080...")
+
+        while True:
+            # Wait for a connection
+            print("Waiting for a connection...")
+            connection, client_address = server_socket.accept()
+            
+            try:
+                print(f"Connection from {client_address}")
+
+                while True:
+                    data = connection.recv(1024)  # Buffer size is 1024 bytes
+                    if data:
+                        message = data.decode('utf-8') # When data is received it is sent in bytes, so we decode it to utf-8
+                        print(f"Received: {message}")
+                        messages.append(message)  # Append to the display list
+                    else:
+                        print(f"Message list: {messages}")
+                        break
+            
+            finally:
+                connection.close()
 
 
 def main():
-    print(create_server_side_connection())
+    print(Server.create_server_side_connection())
 
 if __name__ == "__main__":
     main()
