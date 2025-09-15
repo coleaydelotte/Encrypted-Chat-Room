a89af4b9c6450103f432063c2c0850ef0a37566f
Cole Aydelotte <aydelottec@moravian.edu>
2025-09-14 21:56:06 -0400

    Finished Server launch script.
    Finished Server launch script. :)

--- server_launch.sh
+++ server_launch.sh
@@ -3,3 +3,7 @@ sudo apt-get install -y python3 python3-pip
 sudo apt-get install -f flask
 
 pip3 install flask flask-cors
+
+# now run the python server as a daemon
+nohup python3 server.py &
+echo $! > server.pid
